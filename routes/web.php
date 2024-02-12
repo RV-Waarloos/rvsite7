549fd8045c7b320deccc3ecac8b434c01761f49b
Luc Van Keer <luc.vankeer@telenet.be>
2024-02-12 21:43:52 +0100

    saving

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::statamic('/clubmembers', 'clubmembers/index', [
+   'title' => 'Example'
+]);
